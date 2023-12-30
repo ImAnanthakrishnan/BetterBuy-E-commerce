49c4c3fae1ae203f6eef726b7da0f67dd82f0492
Anantha krishnan <ananthakrishhh2002@gmail.com>
2023-12-30 09:31:30 +0530

    routes updated

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,4 @@
-const express=require('express');
+const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path')
@@ -10,66 +10,108 @@ const userController = require('../controller/admin_userController');
 const inventoryController = require('../controller/inventoryController');
 const orderController = require('../controller/adminOrderController');
 const couponController = require('../controller/adminCouponController');
+const reportController = require('../controller/salesReportController');
+const offerController = require('../controller/adminOfferController');
+const chartController = require('../controller/chartsController');
+const locationController = require('../controller/location_controller');
+const bannerController = require('../controller/bannerController');
+const { route } = require('./user');
 
-router.get('/',auth.isLogout,adminController.loadLogin);
-router.post('/',adminController.verifyLogin);
-router.get('/home',auth.isLogin,adminController.loadDashBoard)
+router.get('/', auth.isLogout, adminController.loadLogin);
+router.post('/',auth.isLogout, adminController.verifyLogin);
+router.get('/home', auth.isLogin, adminController.loadDashBoard)
 
 
 
 router.use(express.static('public'))
 //multer
 const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,path.join(__dirname,'../public/images'))
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public/images'))
     },
-    filename:function(req,file,cb){
-        const name = Date.now()+'-'+file.originalname;
-        cb(null,name);
+    filename: function (req, file, cb) {
+        const name = Date.now() + '-' + file.originalname;
+        cb(null, name);
     }
 })
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 const upload1 = multer();
 //category
-router.get('/category',auth.isLogin,categoryController.loadCategory);
-router.get('/add-category',auth.isLogin,categoryController.addCategoryLoad)
-router.post('/add-category',upload.single('image'),categoryController.addCategory);
-router.get('/edit-category',auth.isLogin,categoryController.editLoadCategory);
-router.post('/edit-category',upload.single('image'),categoryController.updateCategory);
-router.get('/delete-category',auth.isLogin,categoryController.deleteCategory);
+router.get('/category', auth.isLogin, categoryController.loadCategory);
+router.get('/add-category', auth.isLogin, categoryController.addCategoryLoad)
+router.post('/add-category',auth.isLogin, upload.single('image'), categoryController.addCategory);
+router.get('/edit-category', auth.isLogin, categoryController.editLoadCategory);
+router.post('/edit-category',auth.isLogin, upload.single('image'), categoryController.updateCategory);
+router.get('/delete-category', auth.isLogin, categoryController.deleteCategory);
 
 //product
-router.get('/product',auth.isLogin,product_controller.loadProduct)
-router.get('/add-product',auth.isLogin,product_controller.loadAddProduct)
-router.post('/add-product',upload.array('images',3),product_controller.addProduct);
-router.get('/edit-product',auth.isLogin,product_controller.editProductLoad);
-router.post('/edit-product',upload.array('images',3),product_controller.updateProduct)
-router.get('/delete-product',auth.isLogin,product_controller.deleteProduct)
-router.get('/delete-image',auth.isLogin,product_controller.deleteImage);
+router.get('/product', auth.isLogin, product_controller.loadProduct)
+router.get('/add-product', auth.isLogin, product_controller.loadAddProduct)
+router.post('/add-product',auth.isLogin ,upload.array('images', 3), product_controller.addProduct);
+router.get('/edit-product', auth.isLogin, product_controller.editProductLoad);
+router.post('/edit-product',auth.isLogin, upload.array('images', 3), product_controller.updateProduct)
+router.get('/delete-product', auth.isLogin, product_controller.deleteProduct)
+router.get('/delete-image', auth.isLogin, product_controller.deleteImage);
 
 //user
-router.get('/user',auth.isLogin,userController.loadUser);
-router.get('/block-user',auth.isLogin,userController.blockUser);
-router.get('/unblock-user',auth.isLogin,userController.unblockUser);
+router.get('/user', auth.isLogin, userController.loadUser);
+router.get('/block-user', auth.isLogin, userController.blockUser);
+router.get('/unblock-user', auth.isLogin, userController.unblockUser);
 
 
 //inventory
-router.get('/inventory',auth.isLogin,inventoryController.loadInventory);
-router.get('/update-inventory',auth.isLogin,inventoryController.updateInventoryLoad);
-router.patch('/update-inventory',upload1.none(),inventoryController.updateInventory)
+router.get('/inventory', auth.isLogin, inventoryController.loadInventory);
+router.get('/update-inventory', auth.isLogin, inventoryController.updateInventoryLoad);
+router.patch('/update-inventory',auth.isLogin, upload1.none(), inventoryController.updateInventory)
 
 //orders
-router.get('/orders',auth.isLogin,orderController.orderDetails);
-router.patch('/update-status',orderController.changeStatus)
-router.get('/delete-order',auth.isLogin,orderController.deleteOrder);
-router.patch('/refund',orderController.refund);
-//coupons
-router.get('/coupons',auth.isLogin,couponController.couponLoad);
-router.get('/add-coupon',auth.isLogin,couponController.addCouponLoad)
-router.post('/add-coupon',couponController.createCoupon);
-router.get('/edit-coupon',auth.isLogin,couponController.loadEditCoupon);
-router.post('/edit-coupon',couponController.editCoupons);
-router.get('/delete-coupon',auth.isLogin,couponController.deleteCoupons);
+router.get('/orders', auth.isLogin, orderController.orderDetails);
+router.patch('/update-status',auth.isLogin, orderController.changeStatus)
+router.get('/delete-order', auth.isLogin, orderController.deleteOrder);
+router.patch('/refund', orderController.refund);
 
-router.get('/logout',adminController.adminLogout);
+//coupons
+router.get('/coupons', auth.isLogin, couponController.couponLoad);
+router.get('/add-coupon', auth.isLogin, couponController.addCouponLoad)
+router.post('/add-coupon',auth.isLogin, couponController.createCoupon);
+router.get('/edit-coupon', auth.isLogin, couponController.loadEditCoupon);
+router.post('/edit-coupon',auth.isLogin, couponController.editCoupons);
+router.get('/delete-coupon', auth.isLogin, couponController.deleteCoupons);
+
+//report
+router.get('/reports', auth.isLogin, reportController.loadReport);
+router.get('/export-ordersExcel', auth.isLogin, reportController.exportExcelOrders);
+router.get('/export-ordersPdf', auth.isLogin, reportController.exportPdfOrders);
+
+//offers
+router.get('/offers', auth.isLogin, offerController.offerLoad);
+router.get('/add-offers', auth.isLogin, offerController.addOffersLoad);
+router.post('/add-offers',auth.isLogin, offerController.addOffers);
+
+//charts
+//router.get('/dailyBarChart',auth.isLogin,chartController.dailyBarChart);
+router.get('/weeklyBarChart', auth.isLogin, chartController.weeklyBarChart);
+router.get('/monthlyBarChart', auth.isLogin, chartController.monthlyBarChart);
+router.get('/yearlyBarChart', auth.isLogin, chartController.yearlyBarChart);
+
+//router.get('/dailyAreaChart',auth.isLogin,chartController.dailyAreaChart);
+router.get('/weeklyAreaChart', auth.isLogin, chartController.weeklyAreaChart);
+router.get('/monthlyAreaChart', auth.isLogin, chartController.monthlyAreaChart);
+router.get('/yearlyAreaChart', auth.isLogin, chartController.yearlyAreaChart);
+
+//location
+router.get('/location', auth.isLogin, locationController.loadLocation);
+router.get('/add-location', auth.isLogin, locationController.addLocationLoad);
+router.post('/add-location',auth.isLogin, locationController.addLocation);
+router.get('/edit-location', auth.isLogin, locationController.editLocationLoad);
+router.post('/edit-location',auth.isLogin, locationController.editLocation);
+
+//banner
+router.get('/banner', auth.isLogin, bannerController.bannerLoad);
+router.get('/add-banner', auth.isLogin, bannerController.addBannerLoad);
+router.post('/add-banner',auth.isLogin, bannerController.addBanner);
+router.get('/delete-banner',auth.isLogin,bannerController.deleteBanner);
+
+
+router.get('/logout', adminController.adminLogout);
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -14,58 +14,65 @@ const cartController = require('../controller/cartController');
 const checkoutController = require('../controller/checkoutController');
 const couponController = require('../controller/couponController');
 const walletcontroller = require('../controller/walletController');
-
+const invoiceController  = require('../controller/invoiceController');
 
 router.get('/',auth.isLogout,userController.loadLogin);
-router.post('/',userController.verifyLogin,blocked.checkBlocked)
+
+router.post('/',auth.isLogout,userController.verifyLogin,blocked.checkBlocked)
 router.get('/signup',auth.isLogout,userController.loadSignup);
 
 //const urlencoded = express.urlencoded({extended:false});
-router.post('/signup',userController.insertUser);
+router.post('/signup',auth.isLogout,userController.insertUser);
 
 router.get('/verification',auth.isLogout,userController.loadVerification);
 router.post('/verify-otp',auth.isLogout,userController.verifyOtp)
 router.post('/resend-otp',auth.isLogout,userController.resendOtp)
 
 router.get('/forgot',auth.isLogout,userController.forgotLoad);
-router.post('/forgot',userController.forgotVerify)
+router.post('/forgot',auth.isLogout,userController.forgotVerify)
 
 router.get('/forgot-password',auth.isLogout,userController.forgotPasswordLoad)
-router.post('/forgot-password',userController.resetPassword)
+router.post('/forgot-password',auth.isLogout,userController.resetPassword)
 
 
 
 router.get('/home',homeController.loadHome)
 
+router.get('/about',homeController.aboutLoad);
+
+router.get('/contact',homeController.contactLoad);
+
 router.get('/grocery',groceryController.loadGrocery)
 
 router.get('/all',productsController.loadAllProducts);
-router.get('/vegetables',auth.isLogin,productsController.loadVegetables);
-router.get('/fruits',auth.isLogin,productsController.loadFruits);
-router.get('/single-product',productsController.loadSingleProduct);
+router.get('/vegetables',productsController.loadVegetables);
+router.get('/fruits',productsController.loadFruits);
+router.get('/meat',productsController.loadMeat);
+router.get('/breads',productsController.loadBread);
+router.get('/product',productsController.loadSingleProduct);
 
 /*user-profile*/
 router.get('/general',auth.isLogin,accountsController.loadGeneral);
-router.post('/update-profile',accountsController.updateProfile);
+router.post('/update-profile',auth.isLogin, accountsController.updateProfile);
 router.get('/password-settings',auth.isLogin,accountsController.loadChangePassword);
-router.post('/update-password',accountsController.updatePassword);
+router.post('/update-password',auth.isLogin ,accountsController.updatePassword);
 router.get('/address',auth.isLogin,accountsController.addressLoad);
 router.get('/address-settings',auth.isLogin,accountsController.loadChangeAddress);
-router.post('/edit-address',upload.none(),accountsController.editAddress);
-router.get('/delete-address',accountsController.deleteAddress);
+router.post('/edit-address',auth.isLogin,upload.none(),accountsController.editAddress);
+router.get('/delete-address',auth.isLogin,accountsController.deleteAddress);
 router.get('/add-address',auth.isLogin,accountsController.loadAddAddress);
-router.post('/add-address',accountsController.addAdress);
+router.post('/add-address',auth.isLogin,accountsController.addAdress);
 
 router.get('/verification1',auth.isLogin,accountsController.loadVerification);
-router.post('/verify-otp1',accountsController.verifyOtp)
-router.post('/resend-otp1',accountsController.resendOtp)
+router.post('/verify-otp1',auth.isLogin,accountsController.verifyOtp)
+router.post('/resend-otp1',auth.isLogin,accountsController.resendOtp)
 
 //cart
 router.get('/cart',auth.isLogin,cartController.loadCart);
-router.post('/add-cart',upload.none(),cartController.add_to_cart);
-router.post('/add-cart1',upload.none(),cartController.add_to_cart1);
+router.post('/add-cart',auth.isLogin,upload.none(),cartController.add_to_cart);
+router.post('/add-cart1',auth.isLogin,upload.none(),cartController.add_to_cart1);
 router.post('/update-cart',auth.isLogin,cartController.updateCart);
-router.patch('/delete-product',cartController.deleteProduct);
+router.patch('/delete-product',auth.isLogin,cartController.deleteProduct);
 
 //checkout
 router.get('/checkout',auth.isLogin,checkoutController.loadCheckout);
@@ -78,13 +85,14 @@ router.get('/order-placed',auth.isLogin,checkoutController.orderPlaceLoad);
 router.post('/verifyRazorPay',auth.isLogin,upload.none(),checkoutController.razorpayVerify)
 
 //coupon
-router.post('/verifyCoupon',couponController.verifyCoupon);
+router.post('/verifyCoupon',auth.isLogin,couponController.verifyCoupon);
 
 
 //wallet
 router.get('/wallet',auth.isLogin,walletcontroller.loadWallet);
 
-
+//invoice
+router.get('/invoice',auth.isLogin,invoiceController.createInvoice);
 
 router.get('/logout',userController.userLogout);
 module.exports = router;
